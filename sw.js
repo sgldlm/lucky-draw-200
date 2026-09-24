133871a7609d28a6248db6d9f16df735b09e1e7b
sgldlm <sgldlm@users.noreply.github.com>
2026-09-24 06:01:59 +0000

    加入插画：顶栏左侧礼物+赛地金币、右侧奖杯；全屏加纳国旗色彩带飘落

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * - 外部 CDN（Tailwind、图标字体）也会缓存，断网时页面样式不丢
  * 更新网站文件后，把 CACHE_VERSION 改一个新值，用户下次打开会自动换成新版本
  */
-const CACHE_VERSION = 'v13';
+const CACHE_VERSION = 'v14';
 const CACHE_NAME = 'saidi200-' + CACHE_VERSION;
 
 const PRECACHE = [
@@ -17,6 +17,8 @@ const PRECACHE = [
     'icons/icon-512.png',
     'icons/icon-maskable-512.png',
     'icons/apple-touch-icon.png',
+    'images/trophy.svg',
+    'images/gifts.svg',
     'lucky-draw/index.html',
     'lucky-draw/custom-music.js',
     'lucky-draw/round-storage.js',
